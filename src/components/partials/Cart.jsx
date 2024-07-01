72cc4f033e916bedd6b65a165fdc18afe644a24c
Anand patel <panand054@gmail.com>
2024-07-01 16:34:45 +0530

    commit

--- src/components/partials/Cart.jsx
+++ src/components/partials/Cart.jsx
@@ -4,13 +4,19 @@ const Cart = ({ data, title }) => {
   return (
     <div className="">
       {data.map((c, i) => (
-        <Link key={i}>
-            <img src="" alt="" />
+        <Link className="w-[25vh]" key={i}>
+          <img
+            className="h-[45vh] h-[]"
+            src={`https://image.tmdb.org/t/p/original/${
+              c.poste_path || c.backdrop_path
+            }`}
+            alt=""
+          />
           {c.name || c.title || c.original_name || c.original_title}
         </Link>
       ))}
     </div>
-  )
+  );
 };
 
 export default Cart;
